b015ad7f91ec52dcbbef9e4c1ce919025362b55a
ssande-fwd <ssande@forwardedu.com>
2025-06-05 10:54:40 -0400

    remove enum from fwdSensors

--- common/sensors.ts
+++ common/sensors.ts
@@ -2,11 +2,4 @@
 //% color=#5BA42D
 //% icon="\uf1eb"
 //% weight=202
-namespace fwdSensors {
-    export const enum ThresholdDirection {
-        //% block="over"
-        Over,
-        //% block="under"
-        Under,
-    }
-}
+namespace fwdSensors {}
