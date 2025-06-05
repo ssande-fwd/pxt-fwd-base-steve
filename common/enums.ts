37aa25801aa454e0cba2ce78cf6cad0c3652b7ea
ssande-fwd <ssande@forwardedu.com>
2025-06-05 10:27:25 -0400

    enums

--- common/enums.ts
+++ common/enums.ts
@@ -1,8 +1,39 @@
+// If an enum could conceivably be reusable put it here.
+// Else keep it with it's associated module.
+
 namespace fwdEnums {
     export const enum OverUnder {
         //% block="over"
         Over,
         //% block="under"
         Under,
+    }
+
+    export const enum OnOff {
+        //% block="off"
+        Off = 0,
+        //% block="on"
+        On = 1,
+    }
+
+    export const enum ClockwiseCounterclockwise {
+        //% block="↻"
+        CW,
+        //% block="↺"
+        CCW,
+    }
+
+    export const enum ForwardReverse {
+        //% block="forward"
+        Forward = 1,
+        //% block="reverse"
+        Reverse = -1,
+    }
+
+    export const enum RaisedLowered {
+        //% block="raised"
+        Raised = 1,
+        //% block="lowered"
+        Lowered = 0,
     }
 }
